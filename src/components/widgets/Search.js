283922124e65aa892c5957bb03823777d469fbc5
rabiul <rabiul@bitmascot.com>
2019-12-18 12:21:44 +0600

    Sidebar search widget is now functional

--- src/components/widgets/Search.js
+++ src/components/widgets/Search.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Row, Col, Form, FormControl } from 'react-bootstrap';
+import { SearchContext } from '../../contexts/SearchContext';
 
 const SearchWidget = (props) => {
+    let history = useHistory();
+
+    /**
+     * Init Search Keyword State
+     */
+    const [keyword, setKeyword] = useState('');
+    
+    /**
+     * Destructuring SearchContext
+     */
+    const { handleSearch } = useContext( SearchContext );
     return (
         <Row>
             <Col md={12}>
                 <h4>{props.widget.instance.title}</h4>
             </Col>
             <Col md={12}>
-                <form class="form-inline">
-                    <input placeholder="Enter Keywords" type="text" class="mr-sm-2 form-control" />
-                    <button type="button" class="btn btn-outline-info">Search </button>
-                </form>
+                <Form inline onSubmit={ (e) => handleSearch(e, keyword)}>
+                    <FormControl type="text" placeholder="Enter Keywords" className="mr-sm-2" value={keyword} onChange={ (e) => setKeyword(e.target.value) }  />
+                    <input type="submit" className="btn btn-info" value="Search" />
+                </Form>
             </Col>
         </Row>
     );
